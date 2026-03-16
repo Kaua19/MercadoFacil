8d82cb9ab8c726a19ef9722bf4b5566f9b7209c3
Kaua <kaua83707@gmail.com>
2026-03-16 11:42:44 -0300

    criacao da dependencias e mudei o banco de dados de h2 para mysql

---
+++ src/main/java/com/mercadoFacil/demo/MarcadoFacilApplication.java
@@ -0,0 +1,13 @@
+package com.mercadoFacil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcadoFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcadoFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadoFacil/demo/MarcadoFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadoFacil.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcadoFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
